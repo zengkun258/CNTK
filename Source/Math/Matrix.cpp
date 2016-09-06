e2cd1cce5d9e26e2f48307871aa0b443189a6ad6
yuxiaoguo <v-yuxgu@microsoft.com>
2016-09-06 19:53:07 +0800

    fix an extra two lines

--- Source/Math/Matrix.cpp
+++ Source/Math/Matrix.cpp
@@ -1,5 +1,3 @@
-#include "Matrix.h"
-#include "Matrix.h"
 //
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
